bace99ff21bc344e8931539f01528e6da364a7b4
tvkalex <tvkalex@yandex.ru>
2022-12-25 18:59:46 +0300

    Add Seminar8Task4 solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] array3D = new int[2, 2, 2];
+FillArray3D(array3D);
+PrintArray3D(array3D);
+
+
+void FillArray3D(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+void PrintArray3D(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                System.Console.Write($"{array[i, j, k]}({i},{j},{k})" + "\t");
+            }
+        }
+    }
+}
